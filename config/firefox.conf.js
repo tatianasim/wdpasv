4ccc7a88535ce74147192b90b1dc193aec7d60db
TatianaSim <s_tvl@mail.ru>
2021-10-04 22:26:43 +0300

    split config add firefox support add headless support

---
+++ config/firefox.conf.js
@@ -0,0 +1,11 @@
+import merge from 'deepmerge';
+import baseConf from './common/base.conf';
+
+exports.config = merge(baseConf.config, {
+    maxInstances: 1,
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'firefox',
+    }],
+}, { clone: false });
+
---
+++ config/firefox.headless.conf.js
@@ -0,0 +1,14 @@
+import merge from 'deepmerge';
+import baseConf from './common/base.conf';
+
+exports.config = merge(baseConf.config, {
+    maxInstances: 1,
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'firefox',
+        'moz:firefoxOptions': {
+            args:['-headless']
+        },
+    }],
+}, { clone: false });
+
